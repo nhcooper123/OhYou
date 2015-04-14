3faf3cd9605a0bef36efc57581615ea3b0ba1a1c
Natalie Cooper <ncooper@tcd.ie>
2015-04-14 16:08:35 +0100

    Removed defunct fiule

--- R/OhYou_fittingmodels.R
+++
@@ -1,4 +0,0 @@
-#------------------------------------------------------
-# Fitting BM and OU models to simulated trees and data
-# ML and Bayesian models if possible
-#------------------------------------------------------
