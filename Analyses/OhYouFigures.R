6dedc1c9caf50c3555a5cfb140be2725991f6625
Natalie Cooper <ncooper@tcd.ie>
2015-04-29 17:34:53 +0100

    Changed figures as journal wants PDF

--- Analyses/OhYouFigures.R
+++ Analyses/OhYouFigures.R
@@ -9,7 +9,7 @@ ds$evo <- (ds$evolution / ds$evolution.total) * 100
 ds$pal <- (ds$paleo / ds$paleo.total) * 100
 
 # Plot figure 1
-png("Manuscript/Figures/PapersThruTime.png")
+pdf("Manuscript/Figures/OhYou_Figure1.pdf")
 par(bty = "l")
 plot(evo ~ Year, data = ds, las = 1, pch = 16, cex = 1.2, cex.lab = 1.2, cex.axis = 1.2,
      ylab = "OU publications (% of total papers)", xlab = "Year published", ylim = c(0, 0.7))
@@ -34,7 +34,7 @@ ds2$General.use <- ds2$General.use[, drop = TRUE]
 summary.table <- tapply(ds2$Paper, list(ds2$General.use, ds2$Noptima), length)
 
 # Figure 3
-png("Manuscript/Figures/NTaxa.png")
+pdf("Manuscript/Figures/OhYou_Figure2.pdf")
   par(bty = "l")
   hist(ds1$Ntips[which(ds1$Ntips <= 1000)], las = 1, cex.lab = 1.2, cex.axis = 1.2,
        ylab = "Number of papers", xlab = "Number of taxa", ylim = c(0, 80), main = "")
